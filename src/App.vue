39b8f6e65e50c07312dadbb696d06332400327d6
likangde <cander0815@qq.com>
2019-11-29 09:04:23 +0800

    上传代码

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to docker files"/>
+    <HelloWorld msg="Welcome to docker files change"/>
   </div>
 </template>
 
