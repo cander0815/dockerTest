32e1a8bebd0776dbf1701bf13e5d720b011d2ddf
likangde <cander0815@qq.com>
2019-11-29 10:04:03 +0800

    请求测试

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+/* eslint-disable no-console */
+axios.get('/api/get')
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log('err');
+    console.log(err);
+  });
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
